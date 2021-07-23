28715e8ea582cc366eb3c8eb7ff8b20097578e9e
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-07-23 15:21:54 +0300

    Initial Commit

---
+++ Pharmacy/PharmacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PharmacyApp.swift
+//  Pharmacy
+//
+//  Created by Batuhan Baran on 23.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct PharmacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
